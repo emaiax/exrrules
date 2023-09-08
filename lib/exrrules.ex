7232f9a9d9e9c60dd9fb142ccdcefe1c43824953
Eduardo Maia <emaiax@fastmail.com>
2023-09-08 12:54:46 -0300

    initial commit

---
+++ lib/exrrules.ex
@@ -0,0 +1,18 @@
+defmodule Exrrules do
+  @moduledoc """
+  Documentation for `Exrrules`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exrrules.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exrrules_test.exs
@@ -0,0 +1,8 @@
+defmodule ExrrulesTest do
+  use ExUnit.Case
+  doctest Exrrules
+
+  test "greets the world" do
+    assert Exrrules.hello() == :world
+  end
+end
