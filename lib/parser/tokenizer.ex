0913f5fdadfde10415f17198534d22f75f15d21a
Eduardo Maia <emaiax@fastmail.com>
2023-10-21 11:56:03 -0300

    feat: group similar tokens between commas

--- lib/parser/tokenizer.ex
+++ lib/parser/tokenizer.ex
@@ -8,31 +8,41 @@ defmodule Exrrules.Parser.Tokenizer do
   @base_rules Exrrules.Language.English.rules()
   @group_rules ~w(every on at for starting until)a
 
+  def tokenize(input) when input in [nil, ""], do: struct(__MODULE__)
+
   # Function to tokenize the input string
   def tokenize(input) when is_binary(input) do
-    {:ok, tokens} = tokenize(String.downcase(input), [], @base_rules)
+    {:ok, tokens} =
+      input
+      |> String.downcase()
+      |> tokenize([], @base_rules)
+
+    grouped_tokens =
+      tokens
+      |> group_commas()
+      |> group_tokens()
 
     %__MODULE__{
       keywords: Enum.map(tokens, & &1.rule),
-      tokens: group_tokens(tokens)
+      tokens: grouped_tokens
     }
   end
 
-  # Base case: when the input string is empty
-  defp tokenize("", tokens, _keywords) do
+  # Base case: when the input string is finished
+  defp tokenize(nil, tokens, _keywords) do
     {:ok, Enum.reverse(tokens)}
   end
 
   # Match keywords at the beginning of the input string
   defp tokenize(input, tokens, keywords) do
     case find_matching_keyword(input, keywords) do
-      {keyword, rest} ->
-        tokenize(rest, [keyword | tokens], keywords)
-
-      _ ->
-        {:ok, Enum.reverse(tokens), input}
+      {nil, nil} -> {:ok, Enum.reverse(tokens), input}
+      {keyword, rest} -> tokenize(rest, [keyword | tokens], keywords)
     end
   end
+
+  # Helper function to find a matching keyword at the beginning of the string
+  defp find_matching_keyword(nil, _keywords), do: {nil, nil}
 
   # Helper function to find a matching keyword at the beginning of the string
   defp find_matching_keyword(input, keywords) do
@@ -42,7 +52,7 @@ defmodule Exrrules.Parser.Tokenizer do
           case Regex.split(regex, input, include_captures: true, trim: true) do
             # last match, no more tokens
             [match] ->
-              {match, ""}
+              {match, nil}
 
             [match | [rest]] ->
               {match, String.trim(rest)}
@@ -91,6 +101,20 @@ defmodule Exrrules.Parser.Tokenizer do
     group_tokens(rest, keywords, Map.put(result, group, tokens))
   end
 
+  # Recursive case: token is a comma-group
+  defp group_tokens([comma_group | rest], keywords, result) when is_list(comma_group) do
+    current_group = Map.get(result, :current_group)
+
+    group_tokens =
+      result
+      |> Map.get(current_group, [])
+      |> Enum.reverse()
+
+    tokens = Enum.reverse([comma_group | group_tokens])
+
+    group_tokens(rest, keywords, Map.put(result, current_group, tokens))
+  end
+
   # Recursive case: process the next token
   defp group_tokens([token | rest], keywords, result) do
     current_group = Map.get(result, :current_group)
@@ -102,8 +126,8 @@ defmodule Exrrules.Parser.Tokenizer do
 
     # token is a group?
     case Enum.member?(keywords, token.rule) do
+      # token group found, close the current group and start a new one
       true ->
-        # token group found, close the current group and start a new one
         result =
           result
           |> Map.put(token.rule, [])
@@ -111,11 +135,64 @@ defmodule Exrrules.Parser.Tokenizer do
 
         group_tokens(rest, keywords, result)
 
+      # Non-keyword tokens, add it to the current group
       false ->
-        # Non-keyword tokens, add it to the current group
+        # if token is relative, but input is empty, we need to raise an error
+        if token.rule in Exrrules.Language.English.relatives() && Enum.empty?(rest) do
+          raise "Can't find matching token after #{inspect(token.rule)}"
+        end
+
         tokens = Enum.reverse([token | group_tokens])
 
         group_tokens(rest, keywords, Map.put(result, current_group, tokens))
     end
   end
+
+  # Recursive: process all tokens to find the `:comma`s to group
+  #
+  defp group_commas(tokens, acc \\ [])
+
+  defp group_commas([], acc), do: Enum.reverse(acc)
+
+  # Recursive: when comma found, we get the boundaries to check if it's a valid comma group
+  #
+  # - `friday and monday`: [:friday, :comma, :monday]
+  # - `first friday and monday`: [:first, [:friday, :comma, :monday]]
+  # - `first monday and last sunday`: [:first, :monday, :comma, :last, :sunday]
+  # - `first monday and tuesday and last saturday and sunday`: [:first, [:monday, :comma, :tuesday], :comma, :last, [:saturday, :comma, :sunday]]
+  #
+  defp group_commas([%{rule: :comma} = comma | tokens], acc) do
+    # previous_token is in the head of the `acc`
+    {prev_token, acc} = List.pop_at(acc, 0)
+    # next_token is the next after comma in the `tokens`
+    {next_token, tokens} = List.pop_at(tokens, 0)
+
+    if comma_group?(prev_token, next_token) do
+      group_commas(tokens, [List.flatten([prev_token, comma, next_token]) | acc])
+    else
+      # if it's not a comma group, then we must add prev_token and comma to the `acc`
+      # as they'll be processed individually
+      acc =
+        acc
+        |> List.insert_at(0, prev_token)
+        |> List.insert_at(0, comma)
+
+      # add next_token to the `tokens` as it'll be processed in the next iteration
+      group_commas([next_token | tokens], acc)
+    end
+  end
+
+  defp group_commas([token | tokens], acc), do: group_commas(tokens, [token | acc])
+
+  defp comma_group?([prev_token, _comma, next_token], token) do
+    comma_group?(prev_token, token) || comma_group?(next_token, token)
+  end
+
+  defp comma_group?(%{rule: rule}, %{rule: rule}), do: true
+
+  defp comma_group?(%{rule_group: rule_group}, %{rule_group: rule_group})
+       when not is_nil(rule_group),
+       do: true
+
+  defp comma_group?(_prev_token, _next_token), do: false
 end
--- test/parser/tokenizer_test.exs
+++ test/parser/tokenizer_test.exs
@@ -5,11 +5,11 @@ defmodule Exrrules.Parser.TokenizerTest do
 
   @tag :skip
   test "shortcut rules" do
-    assert [:every, :days] = tokenize("daily")
+    assert [:every, :days] = keywords("daily")
   end
 
   test "empty strings" do
-    assert [] = tokenize("")
+    assert [] = keywords("")
   end
 
   test "tokenizer" do
@@ -18,142 +18,142 @@ defmodule Exrrules.Parser.TokenizerTest do
     assert %Tokenizer{
              keywords: [:every, :monday, :at, :time, :comma, :number],
              tokens: %{
+               every: [%Token{rule: :monday, value: "MO", token: "monday"}],
                at: [
                  %Token{rule: :time, value: "10am", token: "10am"},
                  %Token{rule: :comma, value: "and", token: "and"},
-                 %Token{rule: :number, value: "18", token: "18"}
-               ],
-               every: [%Token{rule: :monday, value: "MO", token: "monday"}]
+                 %Token{rule: :number, value: 18, token: "18"}
+               ]
              }
-           } == Tokenizer.tokenize("every monday at 10am and 18")
+           } = Tokenizer.tokenize("every monday at 10am and 18")
   end
 
   test "rule: dates" do
     # yyyy-mm-dd
     #
-    assert [:date] = tokenize("2032-04-20")
-    assert [:date] = tokenize("2032/04/20")
+    assert [:date] = keywords("2032-04-20")
+    assert [:date] = keywords("2032/04/20")
 
     # yyyy-dd-mm
     #
-    assert [:date] = tokenize("2032-20-04")
-    assert [:date] = tokenize("2032/20/04")
+    assert [:date] = keywords("2032-20-04")
+    assert [:date] = keywords("2032/20/04")
 
     # dd-mm-yyyy
     #
-    assert [:date] = tokenize("20-04-2032")
-    assert [:date] = tokenize("20/04/2032")
+    assert [:date] = keywords("20-04-2032")
+    assert [:date] = keywords("20/04/2032")
 
     # mm-dd-yyyy
     #
-    assert [:date] = tokenize("04-20-2032")
-    assert [:date] = tokenize("04/20/2032")
+    assert [:date] = keywords("04-20-2032")
+    assert [:date] = keywords("04/20/2032")
 
     # month dd yyyy
     # month ddth yyyy
     # month ddth, yyyy
     #
-    assert [:date_text] = tokenize("Dec 15 2049")
-    assert [:date_text] = tokenize("Dec 15th 2049")
-    assert [:date_text] = tokenize("Dec 15th, 2049")
+    assert [:date_text] = keywords("Dec 15 2049")
+    assert [:date_text] = keywords("Dec 15th 2049")
+    assert [:date_text] = keywords("Dec 15th, 2049")
 
     # dd month yyyy
     # ddth month yyyy
     # ddth month, yyyy
     #
-    assert [:date_text] = tokenize("15 Dec 2049")
-    assert [:date_text] = tokenize("15th Dec 2049")
-    assert [:date_text] = tokenize("15th Dec, 2049")
+    assert [:date_text] = keywords("15 Dec 2049")
+    assert [:date_text] = keywords("15th Dec 2049")
+    assert [:date_text] = keywords("15th Dec, 2049")
   end
 
   test "rule: time" do
-    assert [:time] = tokenize("9pm")
-    assert [:time] = tokenize("9 am")
-    assert [:time] = tokenize("10 am")
-    assert [:time] = tokenize("12 pm")
+    assert [:time] = keywords("9pm")
+    assert [:time] = keywords("9 am")
+    assert [:time] = keywords("10 am")
+    assert [:time] = keywords("12 pm")
 
-    assert [:time] = tokenize("9:20")
-    assert [:time] = tokenize("09:20")
-    assert [:time] = tokenize("19:30")
-    assert [:time] = tokenize("21:40")
+    assert [:time] = keywords("9:20")
+    assert [:time] = keywords("09:20")
+    assert [:time] = keywords("19:30")
+    assert [:time] = keywords("21:40")
 
-    assert [:time] = tokenize("9:20 am")
-    assert [:time] = tokenize("09:20 pm")
+    assert [:time] = keywords("9:20 am")
+    assert [:time] = keywords("09:20 pm")
 
-    # not valid, but still :time
-    assert [:time] = tokenize("19:30 am")
-    assert [:time] = tokenize("21:40 pm")
-    assert [:time] = tokenize("16 pm")
+    # full format with am/pm is invalid, then we should ignore the time modified (am/pm)
+    assert [:time] = keywords("19:30 am")
+    assert [:time] = keywords("21:40 pm")
+    assert [:time] = keywords("16 pm")
   end
 
   test "rule: frequences" do
-    assert [:every, :days] = tokenize("every day")
-    assert [:every, :days] = tokenize("every days")
-    assert [:every, :weeks] = tokenize("every week")
-    assert [:every, :weeks] = tokenize("every weeks")
-    assert [:every, :months] = tokenize("every month")
-    assert [:every, :months] = tokenize("every months")
-    assert [:every, :years] = tokenize("every year")
-    assert [:every, :years] = tokenize("every years")
+    assert [:every, :days] = keywords("every day")
+    assert [:every, :days] = keywords("every days")
+    assert [:every, :weeks] = keywords("every week")
+    assert [:every, :weeks] = keywords("every weeks")
+    assert [:every, :months] = keywords("every month")
+    assert [:every, :months] = keywords("every months")
+    assert [:every, :years] = keywords("every year")
+    assert [:every, :years] = keywords("every years")
   end
 
   test "rule: intervals" do
-    assert [:every, :other, :days] = tokenize("every other day")
-    assert [:every, :number, :days] = tokenize("every 2 days")
-    assert [:every, :number, :weeks] = tokenize("every 6 weeks")
-    assert [:every, :other, :years] = tokenize("every other year")
-    assert [:every, :other, :weekdays] = tokenize("every other work day")
+    assert [:every, :other, :days] = keywords("every other day")
+    assert [:every, :number, :days] = keywords("every 2 days")
+    assert [:every, :number, :weeks] = keywords("every 6 weeks")
+    assert [:every, :other, :years] = keywords("every other year")
+    assert [:every, :other, :weekdays] = keywords("every other work day")
   end
 
   test "rule: week days" do
-    assert [:every, :monday] = tokenize("every monday")
-    assert [:every, :tuesday] = tokenize("every tuesday")
-    assert [:every, :wednesday] = tokenize("every wednesday")
-    assert [:every, :thursday] = tokenize("every thursday")
-    assert [:every, :friday] = tokenize("every friday")
-    assert [:every, :saturday] = tokenize("every saturday")
-    assert [:every, :sunday] = tokenize("every sunday")
+    assert [:every, :monday] = keywords("every monday")
+    assert [:every, :tuesday] = keywords("every tuesday")
+    assert [:every, :wednesday] = keywords("every wednesday")
+    assert [:every, :thursday] = keywords("every thursday")
+    assert [:every, :friday] = keywords("every friday")
+    assert [:every, :saturday] = keywords("every saturday")
+    assert [:every, :sunday] = keywords("every sunday")
 
-    assert [:every, :weekdays] = tokenize("every weekday")
-    assert [:every, :weekdays] = tokenize("every weekdays")
-    assert [:every, :weekdays] = tokenize("every week day")
-    assert [:every, :weekdays] = tokenize("every week days")
-    assert [:every, :weekdays] = tokenize("every workday")
-    assert [:every, :weekdays] = tokenize("every workdays")
-    assert [:every, :weekdays] = tokenize("every work day")
-    assert [:every, :weekdays] = tokenize("every work days")
+    assert [:every, :weekdays] = keywords("every weekday")
+    assert [:every, :weekdays] = keywords("every weekdays")
+    assert [:every, :weekdays] = keywords("every week day")
+    assert [:every, :weekdays] = keywords("every week days")
+    assert [:every, :weekdays] = keywords("every workday")
+    assert [:every, :weekdays] = keywords("every workdays")
+    assert [:every, :weekdays] = keywords("every work day")
+    assert [:every, :weekdays] = keywords("every work days")
 
-    assert [:every, :weekends] = tokenize("every weekend")
-    assert [:every, :weekends] = tokenize("every weekends")
+    assert [:every, :weekends] = keywords("every weekend")
+    assert [:every, :weekends] = keywords("every weekends")
   end
 
   test "rule: months" do
-    assert [:january, :january] = tokenize("jan january")
-    assert [:february, :february] = tokenize("feb february")
-    assert [:march, :march] = tokenize("mar march")
-    assert [:april, :april] = tokenize("apr april")
-    assert [:may] = tokenize("may")
-    assert [:june, :june] = tokenize("jun june")
-    assert [:july, :july] = tokenize("jul july")
-    assert [:august, :august] = tokenize("aug august")
-    assert [:september, :september, :september] = tokenize("sep sept september")
-    assert [:october, :october] = tokenize("oct october")
-    assert [:november, :november] = tokenize("nov november")
-    assert [:december, :december] = tokenize("dec december")
+    assert [:january, :january] = keywords("jan january")
+    assert [:february, :february] = keywords("feb february")
+    assert [:march, :march] = keywords("mar march")
+    assert [:april, :april] = keywords("apr april")
+    assert [:may] = keywords("may")
+    assert [:june, :june] = keywords("jun june")
+    assert [:july, :july] = keywords("jul july")
+    assert [:august, :august] = keywords("aug august")
+    assert [:september, :september, :september] = keywords("sep sept september")
+    assert [:october, :october] = keywords("oct october")
+    assert [:november, :november] = keywords("nov november")
+    assert [:december, :december] = keywords("dec december")
   end
 
   test "rule: nth" do
-    assert [:nth] = tokenize("1st")
-    assert [:nth, :comma, :nth] = tokenize("1st, 2nd")
-    assert [:nth, :comma, :nth] = tokenize("3rd and 15th")
+    assert [:nth] = keywords("1st")
+    assert [:nth, :comma, :nth] = keywords("1st, 2nd")
+    assert [:nth, :comma, :nth] = keywords("3rd and 15th")
   end
 
   test "rule: relative positions" do
-    assert [:first] = tokenize("first")
-    assert [:second] = tokenize("second")
-    assert [:third] = tokenize("third")
-    assert [:next] = tokenize("next")
-    assert [:last] = tokenize("last")
+    assert [:first, :monday] = keywords("first monday")
+    assert [:second, :tuesday] = keywords("second tuesday")
+    assert [:third, :wednesday] = keywords("third wednesday")
+    assert [:next, :thursday] = keywords("next thursday")
+    assert [:last, :friday] = keywords("last friday")
   end
 
   test "invalid rules" do
@@ -163,37 +163,78 @@ defmodule Exrrules.Parser.TokenizerTest do
       {"every weeksx", ~r{Unsupported rule "weeksx"}},
       {"every monthsx", ~r{Unsupported rule "monthsx"}},
       {"every yearsx", ~r{Unsupported rule "yearsx"}},
+      {"every first", ~r{Can't find matching token after :first}},
+      {"every second", ~r{Can't find matching token after :second}},
+      {"every third", ~r{Can't find matching token after :third}},
+      {"every next", ~r{Can't find matching token after :next}},
+      {"every last", ~r{Can't find matching token after :last}},
       # not a real time, so it doesn't match the time rule
       {"25 pm", ~r{Unsupported rule "pm"}}
     ]
 
     Enum.each(tokens, fn {text, regex} ->
-      assert_raise RuntimeError, regex, fn -> tokenize(text) |> dbg() end
+      assert_raise RuntimeError, regex, fn -> keywords(text) |> dbg() end
     end)
   end
 
-  test "group tokens by keywords" do
+  test "group comma tokens" do
     assert %{
-             every: [%{rule: :days}],
-             at: [%{rule: :number}, %{rule: :comma}, %{rule: :number}]
-           } =
-             "every day at 10 and 15"
-             |> Tokenizer.tokenize()
-             |> Map.get(:tokens)
+             every: [
+               %{rule: :first},
+               [
+                 %{rule: :monday},
+                 %{rule: :comma},
+                 %{rule: :friday},
+                 %{rule: :comma},
+                 %{rule: :sunday}
+               ]
+             ]
+           } = tokens("every first monday, friday and sunday")
+
+    assert %{
+             every: [
+               %{rule: :first},
+               [
+                 %{rule: :monday},
+                 %{rule: :comma},
+                 %{rule: :tuesday}
+               ],
+               %{rule: :comma},
+               %{rule: :last},
+               %{rule: :friday}
+             ],
+             at: [
+               [
+                 %{rule: :number},
+                 %{rule: :comma},
+                 %{rule: :number}
+               ]
+             ]
+           } = tokens("every first monday and tuesday and last friday at 10 and 19")
 
     assert %{
              every: [%{rule: :days}],
-             at: [%{rule: :number}],
-             starting: [%{rule: :on}, %{rule: :the}, %{rule: :nth}]
-           } =
-             "every day at 10 starting on the 15th"
-             |> Tokenizer.tokenize()
-             |> Map.get(:tokens)
+             at: [
+               %{rule: :time},
+               %{rule: :comma},
+               [
+                 %{rule: :number},
+                 %{rule: :comma},
+                 %{rule: :number}
+               ]
+             ]
+           } = tokens("every day at 10am and 14, 18")
   end
 
-  defp tokenize(input, _opts \\ []) do
+  defp keywords(input) do
     input
     |> Tokenizer.tokenize()
     |> Map.get(:keywords)
+  end
+
+  defp tokens(input) do
+    input
+    |> Tokenizer.tokenize()
+    |> Map.get(:tokens)
   end
 end
